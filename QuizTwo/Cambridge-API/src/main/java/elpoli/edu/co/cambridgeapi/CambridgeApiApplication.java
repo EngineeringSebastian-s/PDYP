55f6df46864f91802fc9d5b1a39d2c86bb43b1aa
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-09-16 22:27:15 -0500

    init API and Web projects

---
+++ QuizTwo/Cambridge-API/src/main/java/elpoli/edu/co/cambridgeapi/CambridgeApiApplication.java
@@ -0,0 +1,13 @@
+package elpoli.edu.co.cambridgeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambridgeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CambridgeApiApplication.class, args);
+    }
+
+}
---
+++ QuizTwo/Cambridge-API/src/test/java/elpoli/edu/co/cambridgeapi/CambridgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package elpoli.edu.co.cambridgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambridgeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
